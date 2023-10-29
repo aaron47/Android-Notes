b393aef07de9f0b899207ff5cbf9dd4326884058
Aaron <aaronborgi1@gmail.com>
2023-10-29 21:45:07 +0000

    first commit

---
+++ app/src/main/java/com/aaron/notes/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aaron.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.button = findViewById(R.id.testButton);
+        this.textView = findViewById(R.id.helloWorldText);
+
+        button.setOnClickListener(v -> textView.setText("Text Changed!"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.miAddItem) {
+            Toast.makeText(this, "Vous avez ajoute une note avec success", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+}
