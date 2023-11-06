68136b8b8cb6f215b266b86a1e6db6a038364dd3
Aaron <aaronborgi1@gmail.com>
2023-11-06 10:30:54 +0000

    added favourites badge

--- app/src/main/java/com/aaron/notes/MainActivity.java
+++ app/src/main/java/com/aaron/notes/MainActivity.java
@@ -2,12 +2,11 @@ package com.aaron.notes;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -56,6 +55,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.app_bar_menu, menu);
+
+        MenuItem favouriteItem = menu.findItem(R.id.miFavourite);
+
+        // Set a custom action view for the miFavourite menu item
+        favouriteItem.setActionView(R.layout.badge_layout);
+
+        // Initialize the badge TextView
+        TextView badgeCount = favouriteItem.getActionView().findViewById(R.id.tvFavourites);
+
+        // Update the badge count here with the actual count of items
+        int itemCount = this.notes.size()/* Calculate the count of items */;
+        badgeCount.setText(String.valueOf(itemCount));
+
         return super.onCreateOptionsMenu(menu);
     }
 
